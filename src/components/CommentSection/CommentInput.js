d6baa255a11c3a8f6c9ab00f056083455a5bc8ed
Russell Smith <russellsmith@MacBook-Pro.local>
2020-02-27 00:21:21 -0500

    completed instagram challenge

--- src/components/CommentSection/CommentInput.js
+++ src/components/CommentSection/CommentInput.js
@@ -1,15 +1,23 @@
 // You do not need to do anything in this file
-import React from 'react';
+import React from "react";
 
 const CommentInput = props => {
   return (
-    <form className="comment-form" onSubmit={props.submitComment}>
+    <form
+      style={{ position: "relative" }}
+      className="comment-form"
+      onSubmit={props.submitComment}
+    >
       <input
         type="text"
         value={props.comment}
         placeholder="Add comment... "
         onChange={props.changeComment}
       />
+      <i
+        style={{ position: "absolute", right: "8px", top: "15px" }}
+        className="fas fa-ellipsis-h"
+      ></i>
     </form>
   );
 };
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,18 +1,19 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, { useState } from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+import data from "../../dummy-data.js";
 
 const PostsPage = () => {
-  // set up state for your data
+  const posts = useState(data)[0];
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {posts.map((post, index) => {
+        return <Post key={index} post={post} />;
+      })}
     </div>
   );
 };
 
 export default PostsPage;
-
